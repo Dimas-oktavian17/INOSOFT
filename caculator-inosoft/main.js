d8ed30b9827e645bdc285c4409f96f858eabc336
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-08-20 21:58:28 +0700

    done create caculator

--- caculator-inosoft/main.js
+++ caculator-inosoft/main.js
@@ -1,24 +1,113 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+// Get input field caculator
+const display = document.getElementById("display");
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// Menangani operasi kalkulator
+let firstValue = 0;
+let operator = "";
+let awaitingNextValue = false;
 
-// setupCounter(document.querySelector('#counter'))
+//Fungsi menangani operasi angka
+function handleNumber(number) {
+  if (awaitingNextValue) {
+    display.value = number;
+    awaitingNextValue = false;
+  } else {
+    if (display.value === "0") {
+      display.value = number;
+    } else {
+      display.value += number;
+    }
+  }
+}
+function handleOperator(nextOperator) {
+  var current = display.value;
+  if (operator && awaitingNextValue) {
+    operator = nextOperator;
+    return;
+  }
+  firstValue = parseFloat(current);
+  operator = nextOperator;
+  display.value += operator;
+  awaitingNextValue = true;
+}
+// Fungsi untuk menangani operasi koma
+function handleDecimal() {
+  if (!display.value.includes(".")) {
+    display.value += ".";
+  }
+}
+// Fungsi untuk menangani operasi persen
+function handlePercent() {
+  var current = display.value;
+  current = parseFloat(current);
+  current = current / 100;
+  display.value = current;
+}
+// Fungsi untuk menangani operasi negatif
+function handleNegate() {
+  var current = display.value;
+  current = parseFloat(current);
+  current = -current;
+  display.value = current;
+}
+// Fungsi untuk menangani operasi sama dengan (=)
+function handleEqual() {
+  var current = display.value;
+  current = parseFloat(current);
+  if (operator === "+") {
+    display.value = firstValue + current;
+  } else if (operator === "-") {
+    display.value = firstValue - current;
+  } else if (operator === "*") {
+    display.value = firstValue * current;
+  } else if (operator === "/") {
+    display.value = firstValue / current;
+  }
+  // membersihkan variabel setelah perhitungan selesai
+  firstValue = 0;
+  operator = "";
+  awaitingNextValue = false;
+}
+const buttons = document.getElementsByClassName("number");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    handleNumber(this.textContent);
+  });
+}
+const operatorButtons = document.getElementsByClassName("operator");
+for (let i = 0; i < operatorButtons.length; i++) {
+  operatorButtons[i].addEventListener("click", function () {
+    switch (this.id) {
+      case "clear":
+        display.value = "0";
+        firstValue = 0;
+        operator = "";
+        awaitingNextValue = false;
+        break;
+      case "negate":
+        handleNegate();
+        break;
+      case "percent":
+        handlePercent();
+        break;
+      case "divide":
+        handleOperator("/");
+        break;
+      case "multiply":
+        handleOperator("*");
+        break;
+      case "subtract":
+        handleOperator("-");
+        break;
+      case "add":
+        handleOperator("+");
+        break;
+      case "decimal":
+        handleDecimal();
+        break;
+      case "equals":
+        handleEqual();
+        break;
+    }
+  });
+}
